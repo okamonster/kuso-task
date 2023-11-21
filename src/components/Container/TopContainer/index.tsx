42a218a22df631b0ba071d0feb1a833c6cd53916
okamonster <okamonster0615@gmail.com>
2023-11-21 16:19:01 +0900

    feat:add TopContainer

---
+++ src/components/Container/TopContainer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss'
+
+export const TopContainer = (): React.ReactElement => {
+  return (
+    <div className={styles.topContainer}>
+      <p>hoge</p>
+    </div>
+  )
+}
