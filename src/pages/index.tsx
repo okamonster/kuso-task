3822184bedcc7bfae5abd8a34d24d0c240a9ed8a
okamonster <okamonster0615@gmail.com>
2023-11-22 23:41:22 +0900

    fix:add variable css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { DefaultLayout } from '~/Layouts/DefaultLayout'
 import { TopContainer } from '~/components/Container/TopContainer'
+import { Header } from '~/components/Header'
 
 export default function Home() {
   return (
     <DefaultLayout>
+      <Header>SimpleTask</Header>
       <TopContainer />
     </DefaultLayout>
   )
