1f719e32bf1b164af7ccea316da5570d0085ed14
okamonster <okamonster0615@gmail.com>
2023-11-22 19:21:28 +0900

    feat:propsの追加

--- src/components/Buttons/BasicButton.tsx
+++ src/components/Buttons/BasicButton.tsx
@@ -8,17 +8,25 @@ type Props = {
   varient?: 'primary' | 'secondary' | 'tertiary'
   children: React.ReactNode
   size?: 'sm' | 'md' | 'lg'
+  onClick?: () => void
+  type?: 'button' | 'submit'
 } & ComponentPropsWithoutRef<'button'>
 
 export const BasicButton = ({
   varient = 'primary',
   children,
   size = 'md',
+  onClick,
+  type = 'button',
 }: Props): React.ReactElement => {
   const buttonClass = classNames(
     styles.basicButton,
     styles[varient],
     styles[size],
   )
-  return <button className={buttonClass}>{children}</button>
+  return (
+    <button className={buttonClass} onClick={onClick} type={type}>
+      {children}
+    </button>
+  )
 }
