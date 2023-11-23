7164b57d3e02a660a72312fed1b51b6df61add96
okamonster <okamonster0615@gmail.com>
2023-11-23 14:42:37 +0900

    feat:taskをリストで取得

--- src/components/List/TaskList/index.tsx
+++ src/components/List/TaskList/index.tsx
@@ -3,22 +3,28 @@ import classNames from 'classnames'
 import styles from './style.module.scss'
 
 import { TaskCard } from '~/components/Cards/TaskCard'
+import { Task } from '~/entities/task'
 
 type Props = {
-  status: 'not-start' | 'in-progress' | 'complete'
+  status: 'not-started' | 'in-progress' | 'complete'
+  tasks: Array<Task>
 }
-export const TaskList = ({ status }: Props): React.ReactElement => {
+export const TaskList = ({ status, tasks = [] }: Props): React.ReactElement => {
   const badgeClass = classNames(styles.badge, styles[status])
+
+  const filterdTasks = tasks.filter((task) => task.status === status)
   return (
     <div className={styles.listContainer}>
       <div className={styles.status}>
         <div className={badgeClass}></div>
-        {status === 'not-start' && <p>未着手</p>}
+        {status === 'not-started' && <p>未着手</p>}
         {status === 'in-progress' && <p>進行中</p>}
         {status === 'complete' && <p>完了</p>}
       </div>
       <div className={styles.taskList}>
-        <TaskCard />
+        {filterdTasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
       </div>
     </div>
   )
