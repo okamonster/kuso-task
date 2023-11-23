89c48adaa5fe35dbb90b0663efe45f73e56318cb
okamonster <okamonster0615@gmail.com>
2023-11-23 14:45:49 +0900

    feat:リスト取得

--- src/components/Container/TopContainer/index.tsx
+++ src/components/Container/TopContainer/index.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import styles from './style.module.scss'
 
 import { BasicButton } from '~/components/Buttons/BasicButton'
 import { TaskList } from '~/components/List/TaskList'
 import { Yabai } from '~/components/Yabai'
+import { Task } from '~/entities/task'
+import FetchTasksUseCase from '~/usecases/FetchTasksUseCase'
+
+const fetchTasksUseCase = new FetchTasksUseCase()
 
 export const TopContainer = (): React.ReactElement => {
   const [isYabai, setIsYabai] = useState(false)
+  const [tasks, setTasks] = useState<Array<Task>>([])
+  useEffect(() => {
+    const func = async () => {
+      const tasks = await fetchTasksUseCase.execute()
+      setTasks(tasks)
+    }
+    func()
+  }, [])
+  console.log(tasks)
   return (
     <div className={styles.topContainer}>
       <BasicButton
@@ -17,9 +30,9 @@ export const TopContainer = (): React.ReactElement => {
       >
         Add Task
       </BasicButton>
-      <TaskList status="not-start" />
-      <TaskList status="in-progress" />
-      <TaskList status="complete" />
+      <TaskList status="not-started" tasks={tasks} />
+      <TaskList status="in-progress" tasks={tasks} />
+      <TaskList status="complete" tasks={tasks} />
       {isYabai && <Yabai />}
     </div>
   )
