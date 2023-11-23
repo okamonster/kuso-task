d64ac512c0a2ef2239714c5d3094ae46af50b4c6
okamonster <okamonster0615@gmail.com>
2023-11-23 16:18:51 +0900

    feat:schema定義

--- src/entities/task.ts
+++ src/entities/task.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 export type Task = {
   id: string
   title: string
@@ -12,3 +14,17 @@ export type CreateTaskDto = {
   status: 'not-started' | 'in-progress' | 'completed'
   deadline: Date
 }
+
+export const AddTaskSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'タイトルを入力してください' })
+    .max(20, { message: 'タイトルは20文字以内で入力してください' }),
+  description: z
+    .string()
+    .min(1, { message: '説明を入力してください' })
+    .max(140, { message: '説明は140文字以内で入力してください' }),
+  deadline: z.string().min(1, { message: '期限を入力してください' }),
+})
+
+export type AddTaskSchemaType = z.infer<typeof AddTaskSchema>
